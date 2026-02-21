9ad6a0d046f0d54cfdf74ec5d7f0ebcb1bbf7426
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-02-21 22:16:14 +0600

    validation schema

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { matchRouter } from "./routes/matches";
 
 const app = express();
 
@@ -7,8 +8,9 @@ const port = 8000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello from express");
+  res.send("Hellow from Express!");
 });
+app.get("/matches", matchRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
