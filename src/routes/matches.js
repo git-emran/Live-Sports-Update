425f741bf50e782881e1518efc1eb9a95f13d273
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2026-02-26 21:25:01 +0600

    routes and validation for matches

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -1,7 +1,47 @@
 import { Router } from "express";
+import { createMatchSchema } from "../validation/matches";
+import { matches } from "../db/schema";
+import { db } from "../db/db";
+import { getMatchStatus } from "../utils/match-status";
 
 export const matchRouter = Router();
 
 matchRouter.get("/", (req, res) => {
   res.status(200).json({ message: "Matches List" });
 });
+
+matchRouter.post("/", async (req, res) => {
+  const parsed = createMatchSchema.safeParse(req.body);
+  const {
+    data: { startTime, endTime, homeScore, awayScore },
+  } = parsed;
+
+  if (!parsed.success) {
+    return res.status(400).json({
+      error: "Invalid payload",
+      details: JSON.stringify(parsed.error),
+    });
+  }
+  try {
+    const [event] = await db
+      .insert(matches)
+      .values({
+        ...parsed.data,
+        startTime: new Date(startTime),
+        endTime: new Date(endTime),
+        homeScore: homeScore ?? 0,
+        awayScore: awayScore ?? 0,
+        status: getMatchStatus(startTime, endTime),
+      })
+      .returning();
+
+    res.status(201).json({ data: event });
+  } catch (e) {
+    res.status(500).json({
+      error: "Failed to create match",
+      details: JSON.stringify(e),
+    });
+  }
+});
+
+matchRouter.patch("/", (req, res) => {});
